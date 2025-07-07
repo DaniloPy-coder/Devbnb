1cbb5189287b02c57eeb0d44a7d56ad1667500a3
Danilo <danilodevsp@gmail.com>
2025-07-07 14:22:00 -0300

    projeto devbnb

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import { api } from "../services/api";
 
 export const UserContext = createContext(null);
 
@@ -20,6 +21,21 @@ export const UserContextProvider = ({ children }) => {
       localStorage.setItem("user", JSON.stringify(user));
     } else {
       localStorage.removeItem("user");
+    }
+  }, [user]);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token && !user) {
+      api
+        .get("/me")
+        .then(({ data }) => {
+          setUser(data);
+        })
+        .catch(() => {
+          localStorage.removeItem("token");
+          setUser(null);
+        });
     }
   }, [user]);
 
